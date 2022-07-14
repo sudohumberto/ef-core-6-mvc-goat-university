7928b13901fef9c21ebe9c6a0903ac6997703c26
Humberto Barrantes <sudohumberto@gmail.com>
2022-07-14 00:22:37 -0600

    MVC Project Created

---
+++ GoatUniversity/GoatUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoatUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
