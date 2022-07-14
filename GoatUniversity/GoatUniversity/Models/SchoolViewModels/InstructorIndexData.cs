f5ea8f16573d64b46d3582d123d88866d64662f1
Humberto Barrantes <sudohumberto@gmail.com>
2022-07-14 05:38:50 -0600

    View with Related Data

---
+++ GoatUniversity/GoatUniversity/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace GoatUniversity.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; } = null!;
+        public IEnumerable<Course> Courses { get; set; } = null!;
+        public IEnumerable<Enrollment> Enrollments { get; set; } = null!;
+    }
+}
